5d0134b3351e5f4868cfe70f65ba54fc955e72e0
Rez <reasongurung702@gmail.com>
2026-01-11 17:12:34 +0545

    more questions

--- Assignment(Function)/script.js
+++ Assignment(Function)/script.js
@@ -50,3 +50,163 @@ function factorial(){
     console.log(fact);
 }
 factorial()
+
+// QN6
+function mul5(){
+    console.log("Multiple of 5");
+    let n=5;
+    for(i=1;i<=10;i++){
+    console.log(n*i);
+}
+}
+mul5()
+
+// QN7
+function rev(){
+    console.log("Numbers in reverse order");
+    for(i=10;i>=1;i--){
+    console.log(i);
+    }
+}
+rev()
+
+// QN8
+function mul3(){
+    let n3=3;
+    console.log("Multiple of 3")
+    for(i=1;i<=10;i++){
+    console.log(n3*i);
+    }
+}
+mul3()
+
+// QN9
+function sume(){
+    let sume=0;
+    console.log("Sum of even numbers from 1 to 50");
+    for(i=1;i<=50;i++){
+        if(i%2==0){
+            sume=sume+i;
+        }else{}
+    }
+    console.log(sume);
+}
+sume()
+
+// Qn10
+function sumo(){
+    let sumo=0;
+    console.log("Sum of odd numbers from 1 to 50");
+    for(i=1;i<=50;i++){
+        if(i%2!=0){
+            sumo=sumo+i;
+        }else{}
+    }
+    console.log(sumo);
+}
+sumo()
+
+// QN11
+function mult5(){
+    console.log("Divisible by 5");
+    for(i=1;i<=100;i++){
+        if(i%5==0){
+            console.log(i);
+        }else{}
+    }
+}
+mult5()
+
+// QN12
+function fibo(){
+    console.log("Fibonacci sequence");
+    let a=0;
+    let b=1;
+    for(i=0;i<10;i++){
+        console.log(a);
+        let c=a+b;
+        a=b;
+        b=c;
+    }
+}
+fibo()
+
+// QN13
+
+
+
+
+// QN14
+function square(){
+    for(i=1;i<=10;i++){
+        console.log(i*i);
+    }
+}
+square()
+
+// QN15
+function sum2(){
+    console.log("sum of squares")
+    let n=10;
+    let sums=0;
+    for(i=1;i<=n;i++){
+        sums=sums+i*i;
+    }
+    console.log(sums);
+}
+sum2()
+
+// QN16
+function cube(){
+    console.log("Cube of 1 to 10");
+    for(i=1;i<=10;i++){
+        console.log(i*i*i);
+    }
+}
+cube()
+
+// QN17
+function Smallest(){
+  for (let i=1; i<100; i++) {
+    if (i%3===0 && i%4===0) {
+      return i;
+    }
+  }
+}
+const smallestNumber = Smallest();
+console.log(smallestNumber); 
+
+// QN18
+function even(){
+    console.log("First 10 even numbers");
+    for(i=1;i<=20;i++){
+        if(i%2===0){
+            console.log(i);
+        }
+    }
+}
+even()
+
+// QN19
+function div(){
+    console.log("Divisible by 2 and 5");
+    for(i=1;i<=100;i++){
+        if(i%2===0 && i%5===0){
+            console.log(i);
+        }
+    }
+}
+div()
+
+// QN20
+function div1(){
+    console.log("Divisible by 3 and 5");
+    let sumd=0;
+    for(i=1;i<=100;i++){
+        if(i%3===0 && i%5===0){
+            sumd=sumd+i;
+        }
+    }
+    console.log(sumd);
+}
+div1()
--- Assignment(Loop)/script.js
+++ Assignment(Loop)/script.js
@@ -1,28 +1,33 @@
 // QN1
+console.log("Numbers from 1 to 20");
 for(i=1;i<=20;i++){
     console.log(i);
 }
 
 // QN2
 let sum=0;
+console.log("Sum of first 10 numbers");
 for(i=1;i<=10;i++){
     sum=sum+i;
 }
 console.log(sum);
 
 //QN3
+console.log("Multiple of 5");
 let n=5;
 for(i=1;i<=10;i++){
     console.log(n*i);
 }
 
 //QN4
+console.log("Numbers form 1 to 100");
 for(i=1;i<=100;i++){
     console.log(i);
 }
 
 //QN5
 let sum1=0;
+console.log("Sum of odd numbers");
 for(i=1;i<=10;i++){
     if(i%2!=0){
         sum1=sum1+i;
@@ -31,3 +36,98 @@ for(i=1;i<=10;i++){
     }
 }
 console.log(sum1);
+
+// QN6
+console.log("Count down from 20");
+for(i=20;i>=1;i--){
+    console.log(i);
+}
+
+// QN7
+let n1=3;
+console.log("Multiple of 3");
+for(i=1;i<=10;i++){
+    console.log(n1*i);
+}
+
+// QN8
+console.log("Squares of numbers");
+for(i=1;i<=10;i++){
+    console.log(i*i);
+}
+
+// QN9
+let sum2=0;
+console.log("Sum of first 100 numbers");
+for(i=1;i<=100;i++){
+    sum2=sum2+i;
+}
+console.log(sum2);
+
+// QN10
+console.log("Cube of numbers");
+for(i=1;i<=5;i++){
+    console.log(i*i*i);
+}
+
+// QN11
+console.log("Numbers in reverse order");
+for(i=50;i>=1;i--){
+    console.log(i);
+}
+
+// QN12
+console.log("Count down odd numbers");
+for(i=19;i>=1;i--){
+    if(i%2!=0){
+    console.log(i);
+    }else{}
+}
+
+// QN13
+let sumo=0;
+console.log("Sum of first 5 odd nunumbers")
+for(i=1;i<=10;i++){
+    if(i%2!=0){
+        sumo=sumo+i;
+    }else{}
+}
+console.log(sumo);
+
+// QN14
+console.log(" First 10 Positive Integers");
+for(i=1;i<=10;i++){
+    console.log(i);
+}
+
+// QN15
+console.log("Powers of 2");
+for(i=1;i<=10;i++){
+    console.log(Math.pow(2,i));
+}
+
+// QN16
+let sume=0;
+console.log("Sum of first 10 even numbers");
+for(i=1;i<=20;i++){
+    if(i%2==0){
+        sume=sume+i;
+    }else{}
+}
+console.log(sume);
+
+// QN17
+let n3=4;
+console.log("Multiple of 4")
+for(i=1;i<=10;i++){
+    console.log(n3*i);
+}
+
+// QN18
+
+
+// QN19
+console.log("Count down from 30 to 10");
+for(i=30;i>=10;i--){
+    console.log(i);
+}
