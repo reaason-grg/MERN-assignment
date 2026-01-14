4c85a9b40ca81abd792bf2cf981413d3d79573fe
Rez <reasongurung702@gmail.com>
2026-01-14 20:56:03 +0545

    Array 1 assignment

---
+++ Assignment(Array 1)/script.js
@@ -0,0 +1,118 @@
+// QN1
+let numbers=[1,2,3,4];
+numbers.push(7);
+// console.log(numbers)
+
+// QN2
+let arr=["apple","banana"];
+arr.pop();
+// console.log(arr);
+
+// QN3
+let arr1=[10,20,30,40];
+arr1.shift(7);
+// console.log(arr1);
+
+// QN4
+function array(){
+    let arr4=["middle","end"];
+    arr4.unshift("start");
+    // console.log(arr4);
+}
+array()
+
+// QN5
+let arr5=["one","two","three"];
+let newarr=arr5.toString();
+// console.log(newarr);
+
+// QN6
+let arr6=["cat","dog"];
+let newarr6=arr6.concat(["fish","bird"]);
+// console.log(newarr6);
+
+// QN7
+let arr7=["red","green","blue"];
+let newarr7=arr7.slice(0,2);
+// console.log(newarr7);
+
+// QN8
+function array8(){
+    let arr8=["x","y","z"];
+    arr8.pop();
+    arr8.unshift("a");
+    // console.log(arr8);
+}
+array8();
+
+// QN9
+let arr9=[100,200,300];
+arr9.toString;
+x=arr9.length;
+// console.log(x);
+
+// QN10
+let arr10=["apple","banana"];
+arr10.push("orange");
+arr10.push("grape");
+// console.log(arr10);
+
+// QN11
+let arr11=["sun","moon","stars"];
+arr11.shift();
+// console.log(arr11);
+
+// QN12
+let arr12=[7,14,21];
+arr12.unshift(42);
+// console.log(arr12);
+
+// QN13
+let arr13=[5,10,15,20];
+let y=arr13.pop();
+// console.log(y);
+
+// QN14
+let arr14=["hello","world"];
+let newarr14=arr14.toString();
+// console.log(newarr14);
+let newarr141=arr14.slice(0,2);
+// console.log(newarr141);
+
+// QN15
+let arr151=["x","y"];
+let arr152=[1,2];
+let newarr15=arr151.concat(arr152);
+// console.log(newarr15);
+
+// QN16
+let arr16=[1,2,3,4];
+arr16.push(99);
+arr16.shift();
+// console.log(arr16);
+
+// QN17
+let arr17=["a","b","c","d"];
+let newarr17=arr17.slice(2,4);
+// console.log(newarr17);
+
+// QN18
+let arr18=["elephant","girrafe"];
+arr18.unshift("lion","tiger");
+console.log(arr18);
+x=arr18.length;
+console.log(x);
+
+// QN19
+let arr19=[20,30,40];
+let newarr19=arr19.toString();
+console.log(newarr19);
+let newarr191=arr19.slice(0,3);
+console.log(newarr191);
+
+// QN20
+let arr20=["apple","banana","orange"];
+arr20.push("grape");
+console.log(arr20);
+arr20.pop();
+console.log(arr20);
